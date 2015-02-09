8f008fad79af7e70b171b59aa6e25f12039f579d
Saptarshi <sunbiz@gmail.com>
2015-02-09 10:33:45 -0500

    login and toolbar changes

--- www/scripts/login.js
+++ www/scripts/login.js
@@ -3,7 +3,7 @@ function validateAll() {
     var $d = document.getElementById('validate').querySelectorAll('paper-input-decorator');
     Array.prototype.forEach.call($d, function (d) {
         d.isInvalid = !d.querySelector('input').validity.valid;
-        if(d.isInvalid){
+        if (d.isInvalid) {
             allValid = false;
         }
     });
@@ -11,20 +11,28 @@ function validateAll() {
 }
 
 function loginToApp() {
-    if(validateAll() === true){
-        console.log('do login');
-        window.location = 'pages/homepage.html';
+    if (validateAll() === true) {
+        username = document.getElementById('username').value;
+        password = document.getElementById('password').value;
+        if (username == 'admin' && password == 'test') {
+            console.log('do login');
+            window.location = 'pages/homepage.html';
+        } else {
+            document.getElementById('loginMsg').style.display = 'block';
+        }
     }
 }
 
 var scope = document.querySelector('template[is=auto-binding]');
 
 scope.userValidate = function (e) {
+    document.getElementById('loginMsg').style.display = 'none';
     var d = document.getElementById('usernameDeco');
     d.isInvalid = !e.target.validity.valid;
 };
 
 scope.passValidate = function (e) {
+    document.getElementById('loginMsg').style.display = 'none';
     var d = document.getElementById('passwordDeco');
     d.isInvalid = !e.target.validity.valid;
 };
